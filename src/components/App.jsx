82383a411e67ce67bd0e780f37e422213ea98306
malishubham025 <malishubham025@gmail.com>
2024-06-18 01:34:51 +0530

    stateless

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,26 @@
 import React from "react";
+// import {BrowserRouter as Router,Routes,Route,Navigate,Link} from "react-router-dom"
+
+function Home(){
+  return (
+    <div>
+      <h2>hi 2</h2>
+     
+      
+    </div>
+  );
+  
+  
+}
+function Home2(){
+  return <h2>hi 2</h2>
+}
+function Fun({child}){
+  return 1?child:null;
+}
 function App(){
-  return <h1>Hello</h1>
+  return(
+  <h1>App</h1>
+  );
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import Home from './components/Home';
+import { BrowserRouter as Router,Routes,Route,Navigate } from 'react-router-dom';
+import axios from 'axios';
+axios.defaults.withCredentials=true;
+function Home2()
+{   
+    const user={username:"abc",password:"123"}
+    axios.post("http://localhost:4000/login",user,{withCredentials:true}).then((res)=>{
+        console.log(res);
+    }).catch((err)=>{
+        console.log(err);
+    })
+    // <Navigate to="/home"/>
 
-ReactDOM.render(<App/>,document.querySelector(".root"))
+    return <h1>hello2</h1>
+   
+}
+function Protected({children}){
+    const [set,Setchild]=React.useState(null);
+    React.useEffect(()=>{
+        axios.get("http://localhost:4000/user").then((res)=>{
+            if(res.status==200){
+                Setchild(true);
+            }
+            else{
+                Setchild(false);
+            }
+            
+        }).catch((err)=>{
+            Setchild(false);
+        })
+    },[]);
+   
+    // .catch((err)=>{
+        // return <h1>hello page</h1>;
+    // })
+    return set?children:<h1> hello there  </h1>
+    
+}
+function App2(){
+    return(
+        <Router>
+            <Routes>
+                <Route path="/" element={<App/>}/>
+                <Route path="/home" element={
+                    <Protected>
+                        <Home/>
+                    </Protected>
+                    }/>
+                <Route path="/home2" element={<Home2/>}/>
+            </Routes>
+        </Router>
+    );
+}
+ReactDOM.render(<App2/>,document.querySelector(".root"))
 
